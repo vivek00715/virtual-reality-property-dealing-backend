781bd91ddd3d20ef4addc9b8468972569ab4d5d7
Vivek Shukla <vivek.shukla@hashedin.com>
2021-04-20 13:31:32 +0000

    Property and city details

--- src/main/java/com/hashedin/virtualproperty/application/repository/PropertyRepo.java
+++ src/main/java/com/hashedin/virtualproperty/application/repository/PropertyRepo.java
@@ -11,32 +11,42 @@ import java.util.List;
 @Repository
 public interface PropertyRepo extends CrudRepository<Property,Integer>, JpaRepository<Property,Integer> {
 
-    @Query("select p from Property p")
-    List<Property> getProperties();
+    @Query("select p from Property p where " +
+            "p.price>=?1 and p.price<=?2 and " +
+            "p.address like %?3 and p.city like %?4 and p.state like %?5 and " +
+            "p.type like %?6 and p.purpose like %?7")
+    List<Property> getProperties(int minPrice,int maxPrice,String address, String city,String state,
+                                 String type,String purpose );
 
-    @Query("select p from Property p where p.city like %?1")
-    List<Property> getPropertiesByCity(String city);
-
-    @Query("select p from Property p where p.city like %?1 and p.type like %?2")
-    List<Property> getPropertiesByCityAndType(String city,String type);
-
-    @Query("select p from Property p where p.state like %?1 and p.type like %?2")
-    List<Property> getPropertiesByStateAndType(String state,String type);
-
-    @Query("select p from Property p where p.address like %?1 and p.city like %?2 and p.state like %?3")
-    List<Property> getPropertiesByAddress(String address,String city,String state);
-
-    @Query("select p from Property p where p.state like %?1")
-    List<Property> getPropertiesByState(String state);
+    @Query("select p from Property p where p.ownerEmail=?1")
+     List<Property> getOwnerProperty(String email);
 
     @Query("select p from Property p where propertyId =?1")
-    Property getPropertiesById(Integer id);
+     Property getPropertiesById(Integer id);
 
-    @Query("select p from Property p where p.price>=?1 and p.price<=?2")
-    List<Property> getPropertiesByBudget(int minPrice,int maxPrice);
+//    @Query("select p from Property p where p.city like %?1")
+//    List<Property> getPropertiesByCity(String city);
+//
+//    @Query("select p from Property p where p.city like %?1 and p.type like %?2")
+//    List<Property> getPropertiesByCityAndType(String city,String type);
+//
+//    @Query("select p from Property p where p.state like %?1 and p.type like %?2")
+//    List<Property> getPropertiesByStateAndType(String state,String type);
+//
+//    @Query("select p from Property p where p.address like %?1 and p.city like %?2 and p.state like %?3")
+//    List<Property> getPropertiesByAddress(String address,String city,String state);
+//
+//    @Query("select p from Property p where p.state like %?1")
+//    List<Property> getPropertiesByState(String state);
+//
+//    @Query("select p from Property p where p.price>=?1 and p.price<=?2")
+//    List<Property> getPropertiesByBudget(int minPrice,int maxPrice);
+//
+//    @Query("select p from Property p where p.price<=?1")
+//    List<Property> getPropertiesByMaxPrice(int maxPrice);
 
-    @Query("select p from Property p where p.price<=?1")
-    List<Property> getPropertiesByMaxPrice(int maxPrice);
+//    @Query("select p from Property p")
+//    List<Property> getProperties();
 
 
 }
--- src/main/java/com/hashedin/virtualproperty/application/service/PropertyService.java
+++ src/main/java/com/hashedin/virtualproperty/application/service/PropertyService.java
@@ -14,66 +14,24 @@ public class PropertyService {
     @Autowired
     private PropertyRepo propertyRepo;
 
-    public Property createProperty(Property property)
+    @Autowired
+    private AuthService authService;
+
+    public Property createProperty(Property property,String token)
     {
-        propertyRepo.save(property);
-        return new Property(property.getPropertyId(),property.getCity(), property.getState(), property.getAddress(),property.getType(),property.getPurpose(),property.getOwnerEmail(),property.getDescription(),property.getArea(),property.getBhk(),property.getBuiltYear(),property.getPrice(),property.getFloors(),property.getBedrooms(),property.getBathrooms(),property.getPinCode());
+        property.setOwnerEmail(authService.getUserEmailFromToken(token));
+        return propertyRepo.save(property);
     }
 
-    public List<Property> getProperty()  {
+    public List<Property> getProperty(int minPrice,int maxPrice,String street,String city,String state,String type,String purpose)  {
 
-          List<Property> propertyList=propertyRepo.getProperties();
+        List<Property> propertyList=propertyRepo.getProperties(minPrice,maxPrice,street,city,state,
+                                                               type,purpose);
           if(propertyList.isEmpty())
               throw new CustomException("No Property Found....");
           return propertyList;
 
     }
-
-    public List<Property> getPropertyByCityName(String city) {
-
-        List<Property> propertyList=propertyRepo.getPropertiesByCity(city);
-
-        if(propertyList.isEmpty())
-            throw new CustomException("No Property Found in city "+city);
-        return propertyList;
-    }
-
-    public List<Property> getPropertyByCityAndType(String city,String type)  {
-
-        List<Property> propertyList=propertyRepo.getPropertiesByCityAndType(city,type);
-
-        if(propertyList.isEmpty())
-            throw new CustomException("No Property Found in city "+city+" of type "+type);
-        return propertyList;
-    }
-
-    public List<Property> getPropertyByStateAndType(String state,String type)  {
-
-        List<Property> propertyList=propertyRepo.getPropertiesByStateAndType(state,type);
-
-        if(propertyList.isEmpty())
-            throw new CustomException("No Property Found in city "+state+" of type "+type);
-        return propertyList;
-    }
-
-    public List<Property> getPropertyByAddress(String address,String city,String state)  {
-
-        List<Property> propertyList=propertyRepo.getPropertiesByAddress(address,city,state);
-
-        if(propertyList.isEmpty())
-            throw new CustomException("No Property Found in address "+address+" in city "+city);
-        return propertyList;
-    }
-
-    public List<Property> getPropertyByState(String state)  {
-
-        List<Property> propertyList=propertyRepo.getPropertiesByState(state);
-
-        if(propertyList.isEmpty())
-            throw new CustomException("No Property Found in state "+state);
-        return propertyList;
-    }
-
     public Property getPropertyById(Integer propertyId)  {
 
         Property property=propertyRepo.getPropertiesById(propertyId);
@@ -82,24 +40,77 @@ public class PropertyService {
             throw new CustomException("No Property Found with id "+propertyId);
         return property;
     }
+    public List<Property> getOwnerProperty(String email)  {
 
-    public List<Property> getPropertyByBudget(int minPrice,int maxPrice) {
+        List<Property> property=propertyRepo.getOwnerProperty(email);
 
-        List<Property> property=propertyRepo.getPropertiesByBudget(minPrice,maxPrice);
-
-        if(property.isEmpty())
-            throw new CustomException("No Property Found from budget "+minPrice+" to "+maxPrice);
+        if(property==null)
+            throw new CustomException("No Property Found ");
         return property;
     }
 
-    public List<Property> getPropertyByMaxPrice(int maxPrice)  {
-
-        List<Property> property=propertyRepo.getPropertiesByMaxPrice(maxPrice);
-
-        if(property.isEmpty())
-            throw new CustomException("No Property Found less than "+maxPrice);
-        return property;
-    }
+//    public List<Property> getPropertyByCityName(String city) {
+//
+//        List<Property> propertyList=propertyRepo.getPropertiesByCity(city);
+//
+//        if(propertyList.isEmpty())
+//            throw new CustomException("No Property Found in city "+city);
+//        return propertyList;
+//    }
+//
+//    public List<Property> getPropertyByCityAndType(String city,String type)  {
+//
+//        List<Property> propertyList=propertyRepo.getPropertiesByCityAndType(city,type);
+//
+//        if(propertyList.isEmpty())
+//            throw new CustomException("No Property Found in city "+city+" of type "+type);
+//        return propertyList;
+//    }
+//
+//    public List<Property> getPropertyByStateAndType(String state,String type)  {
+//
+//        List<Property> propertyList=propertyRepo.getPropertiesByStateAndType(state,type);
+//
+//        if(propertyList.isEmpty())
+//            throw new CustomException("No Property Found in city "+state+" of type "+type);
+//        return propertyList;
+//    }
+//
+//    public List<Property> getPropertyByAddress(String address,String city,String state)  {
+//
+//        List<Property> propertyList=propertyRepo.getPropertiesByAddress(address,city,state);
+//
+//        if(propertyList.isEmpty())
+//            throw new CustomException("No Property Found in address "+address+" in city "+city);
+//        return propertyList;
+//    }
+//
+//    public List<Property> getPropertyByState(String state)  {
+//
+//        List<Property> propertyList=propertyRepo.getPropertiesByState(state);
+//
+//        if(propertyList.isEmpty())
+//            throw new CustomException("No Property Found in state "+state);
+//        return propertyList;
+//    }
+//
+//    public List<Property> getPropertyByBudget(int minPrice,int maxPrice) {
+//
+//        List<Property> property=propertyRepo.getPropertiesByBudget(minPrice,maxPrice);
+//
+//        if(property.isEmpty())
+//            throw new CustomException("No Property Found from budget "+minPrice+" to "+maxPrice);
+//        return property;
+//    }
+//
+//    public List<Property> getPropertyByMaxPrice(int maxPrice)  {
+//
+//        List<Property> property=propertyRepo.getPropertiesByMaxPrice(maxPrice);
+//
+//        if(property.isEmpty())
+//            throw new CustomException("No Property Found less than "+maxPrice);
+//        return property;
+//    }
 
 
 
