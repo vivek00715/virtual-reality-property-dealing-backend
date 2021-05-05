3bb536e3f3c42f59ecafc49ef8e0b93cfdd10cff
Harshit Pal <harshit.pal@hashedin.com>
2021-05-05 10:13:58 +0530

    Added fixes to run locally

--- src/main/java/com/hashedin/virtualproperty/application/service/DbSeedService.java
+++ src/main/java/com/hashedin/virtualproperty/application/service/DbSeedService.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Locale;
-import java.util.Set;
 
 @Service
 public class DbSeedService {
