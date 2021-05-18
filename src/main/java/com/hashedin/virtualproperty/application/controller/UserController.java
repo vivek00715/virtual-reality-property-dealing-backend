22f838049f636abe8222ee13bfaaeacca01f1469
Harshit Pal <harshit.pal@hashedin.com>
2021-05-18 11:23:00 +0530

    Added route for user image upload

--- src/main/java/com/hashedin/virtualproperty/application/controller/UserController.java
+++ src/main/java/com/hashedin/virtualproperty/application/controller/UserController.java
@@ -1,12 +1,13 @@
 package com.hashedin.virtualproperty.application.controller;
 
-import com.hashedin.virtualproperty.application.entities.User;
 import com.hashedin.virtualproperty.application.request.UserUpdateRequest;
 import com.hashedin.virtualproperty.application.response.UserResponse;
 import com.hashedin.virtualproperty.application.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 public class UserController {
@@ -15,12 +16,20 @@ public class UserController {
     private UserService userService;
 
     @GetMapping(value = "/user/{userEmail}")
-    public UserResponse getUserByID(@PathVariable String userEmail , @RequestHeader(name="Authorization") String token){
-        return userService.getUserDetail(userEmail , token);
+    public UserResponse getUserByID(@PathVariable String userEmail, @RequestHeader(name = "Authorization") String token) {
+        return userService.getUserDetail(userEmail, token);
     }
 
-    @PutMapping(value="/user/")
-    public UserResponse updateUserById(@RequestBody UserUpdateRequest user, @RequestHeader(name="Authorization") String token){
-        return userService.updateUserById(user , token);
+    @PutMapping(value = "/user/")
+    public UserResponse updateUserById(@RequestBody UserUpdateRequest user, @RequestHeader(name = "Authorization") String token) {
+        return userService.updateUserById(user, token);
+    }
+
+    @PostMapping("/user/image")
+    public UserResponse addImage(
+            @RequestHeader(name = "Authorization", defaultValue = "") String token,
+            @RequestParam("image") MultipartFile image
+    ) throws IOException {
+        return this.userService.addImage(image, token);
     }
 }
--- src/main/java/com/hashedin/virtualproperty/application/service/UserService.java
+++ src/main/java/com/hashedin/virtualproperty/application/service/UserService.java
@@ -5,13 +5,16 @@ import com.hashedin.virtualproperty.application.entities.User;
 import com.hashedin.virtualproperty.application.exceptions.CustomException;
 import com.hashedin.virtualproperty.application.repository.UserRepository;
 import com.hashedin.virtualproperty.application.request.UserUpdateRequest;
+import com.hashedin.virtualproperty.application.response.FileResponse;
 import com.hashedin.virtualproperty.application.response.UserResponse;
 import org.hibernate.service.spi.InjectService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.Optional;
 
 @Service
@@ -23,23 +26,41 @@ public class UserService {
     @Autowired
     private AuthService authService;
 
+    @Autowired
+    private FileStorageService fileStorageService;
+
     private final Logger logger = LoggerFactory.getLogger(UserService.class);
 
-    public UserResponse getUserDetail(String email, String token){
+    public UserResponse getUserDetail(String email, String token) {
         User u = this.authService.getUserFromToken(token);
         this.logger.info("SENDING INFORMATION OF USER WITH EMAIL " + email);
-        return new UserResponse(u.getName() , u.getAddress() , u.getUserImage() , u.getMobile() , u.getEmail());
+        return new UserResponse(u.getName(), u.getAddress(), u.getUserImage(), u.getMobile(), u.getEmail());
 
     }
 
-    public UserResponse updateUserById(UserUpdateRequest user, String token){
+    public UserResponse addImage(MultipartFile image, String token) throws IOException {
+        User user = this.authService.getUserFromToken(token);
+        this.logger.info("Updating image for user: " + user.getUserImage());
+        FileResponse file = this.fileStorageService.storeFile(image);
+        user.setUserImage(file.url);
+        User savedUser = this.userRepository.save(user);
+        return new UserResponse(
+                savedUser.getName(),
+                savedUser.getAddress(),
+                savedUser.getUserImage(),
+                savedUser.getMobile(),
+                savedUser.getEmail()
+        );
+    }
+
+    public UserResponse updateUserById(UserUpdateRequest user, String token) {
         User u1 = this.authService.getUserFromToken(token);
         u1.setName(user.name);
         u1.setAddress(user.address);
         u1.setMobile(user.mobile);
         User u = userRepository.save(u1);
         this.logger.info("UPDATING USER WITH EMAIL " + u1.getEmail());
-        return new UserResponse(u.getName() , u.getAddress() , u.getUserImage() , u.getMobile() , u.getEmail());
+        return new UserResponse(u.getName(), u.getAddress(), u.getUserImage(), u.getMobile(), u.getEmail());
 
     }
 }
